108c841c090b65332409237ad7b9f7d82d195558
Jaures <jauresade@gmail.com>
2018-05-22 19:35:28 -0700

    Created header and cpp files for sergin class.

---
+++ include/sergin.h
@@ -0,0 +1,21 @@
+/**
+    Author:     Jaures Ade
+    Email:      jauresade@gmail.com
+    File:       sergin.h
+    Date:       05/22/2017
+    
+    Description:
+        Contain Sergin Class declaration for handling logic of storing and 
+        retrieving information of the database and computing trends and patterns.
+
+*/
+
+class Sergin
+{
+    private:
+    public:
+        // Default Constructor
+        Sergin();
+        // Overloaded Constructor
+        Sergin(String);
+};
---
+++ src/sergin.cpp
@@ -0,0 +1,11 @@
+/**
+    Author:     Jaures Ade
+    Email:      jauresade@gmail.com
+    File:       sergin.h
+    Date:       05/22/2017
+    
+    Description:
+        Contain Sergin Class defitions for handling logic of storing and 
+        retrieving information of the database and computing trends and patterns.
+
+*/

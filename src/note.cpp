6add166b2c09425b290fa706e5489fe217b25719
Jaures <jauresade@gmail.com>
2018-05-22 20:23:59 -0700

    Created Header and Source files for the Note Class

---
+++ src/note.cpp
@@ -0,0 +1,13 @@
+/**
+    Author:     Jaures Ade
+    Email:      jauresade@gmail.com
+    File:       note.cpp
+    Date:       05/22/2017
+    
+    Description:
+        Source file for Note Class that is the base class for all possible
+        type of entries in the database. Each Note has a unique ID, a title,
+        a description of the note, tags associated with the note, and the date
+        the note was created.
+
+*/
